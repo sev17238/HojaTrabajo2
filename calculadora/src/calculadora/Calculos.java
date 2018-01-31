b8904ce656e1ec9a3116cc3c14aa3e60c398bf26
GuilleLink <32148569+GuilleLink@users.noreply.github.com>
2018-01-30 18:51:11 -0600

    Progra de defensa contra division entre 0 y comentarios (parte del Javadoc)

--- calculadora/src/calculadora/Calculos.java
+++ calculadora/src/calculadora/Calculos.java
@@ -37,7 +37,15 @@ public class Calculos implements Calculadora{
                     //int num = Double.parseDouble(caracter);
                     //if (num>=0 && num<=9){
                         //vector.push(caracter);
-                    }else if(caracter.contentEquals("+")){
+                    }
+                    //Si por el contrario el usuario ingresa algun operando de los 
+                    //Basicos deberia de efectuar tal operacion y lo hace con una 
+                    //comparacion de Strings
+                    //Cabe notar que en los Stacks solamente se puede sacar el 
+                    //Ultimo valor asi que para operar se guarda este valor de forma
+                    //Temporal en una variable para que asi de esta forma
+                    //
+                    else if(caracter.contentEquals("+")){
                         result = Double.parseDouble(vector.pop());
                         double num2 = Double.parseDouble(vector.pop());
                         result = suma(num2,result);
@@ -53,10 +61,20 @@ public class Calculos implements Calculadora{
                         result = producto(result,num2);
                         vector.push(Double.toString(result));
                     }else if(caracter.contentEquals("/")){
+                        //La division tiene una condicion en la cual si el 
+                        //Ultimo valor del vector es igual a 0 definitivamente no
+                        //sera capaz de efectuar la division por lo que tirara
+                        //al usuario una alerta de que es invalido dividir entre 0.
+                        result = Double.parseDouble(vector.peek());
+                        if(result == 0){
+                            System.out.println("Error, no se puede dividir entre 0");
+                        }
+                        else {
                         result = Double.parseDouble(vector.pop());
                         double num2 = Double.parseDouble(vector.pop());
                         result = division(num2,result);
                         vector.push(Double.toString(result));
+                        }       
                     }else{
                         //System.out.println(caracter);
                     }            
@@ -72,18 +90,25 @@ public class Calculos implements Calculadora{
     }
     
     private double suma(double a,double b){
+        //El metodo que suma dos valores en estilo Infix
         double result = a + b;
         return result;
     }
     private double resta(double a,double b){
+        //El metodo que se ocupa de restar los dos valores ingresados en el orden
+        //en el que fueron ingresados
         double result = a - b;
         return result;
     }
     private double producto(double a,double b){
+        //El metodo que multiplica los dos valores ingresados y regresa su resultado
         double result = a * b;
         return result;
     }
     private double division(double a,double b){
+        //Este metodo se ocupa de dividir, sin embargo su defensa de dividir cualquier
+        //numero entre 0 se encuentra directamente en el el metodo antes de utilizar
+        //Esta operacion, de tal manera no llega a esta linea con ese problema
         double result = a/b;
         return result;
     }
